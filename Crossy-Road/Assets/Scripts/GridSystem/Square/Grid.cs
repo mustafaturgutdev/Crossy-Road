c15c8a3cf544e2f781760be2f55812d59ad6d316
mustafa turgut <mustafaturgut2153@gmail.com>
2023-10-27 15:09:25 +0300

    update grid

--- Crossy-Road/Assets/Scripts/GridSystem/Square/Grid.cs
+++ Crossy-Road/Assets/Scripts/GridSystem/Square/Grid.cs
@@ -109,4 +109,54 @@ namespace GridSystem.Square
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
+
+    public class Grid<T> : IEnumerable<T> where T : IPlacable
+    {
+        private readonly Dictionary<Cell, T> _CellPlacablePairs = new();
+
+        public Grid(Grid grid)
+        {
+            foreach (Cell cell in grid)
+                _CellPlacablePairs.Add(cell, default);
+        }
+
+        public bool TryGetValue(Cell cell, out T value)
+        {
+            return _CellPlacablePairs.TryGetValue(cell, out value) && value != null;
+        }
+
+        public bool Place(T value, Cell cell)
+        {
+
+            if (_CellPlacablePairs.TryGetValue(cell, out _))
+            {
+                value.Cell = cell;
+                _CellPlacablePairs[cell] = value;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool DisPlace(T value)
+        {
+            if (value != null && value.Cell != null && _CellPlacablePairs.TryGetValue(value.Cell, out _))
+            {
+                _CellPlacablePairs[value.Cell] = default;
+                value.Cell = default;
+                return true;
+            }
+
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var pair in _CellPlacablePairs)
+                if (pair.Value != null)
+                    yield return pair.Value;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
 }
